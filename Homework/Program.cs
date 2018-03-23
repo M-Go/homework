7c3dd6fba28bc304d886b8e9a91f7cef6813e75a
mariia.gorobets <mariia.gorobets@betlab.com>
2018-03-23 13:02:15 +0200

    Initial commit

---
+++ Homework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BookStore book = new BookStore(100, true);
+            BookStore magazine = new BookStore(50, false, "glossy paper");
+            BookStore newspaper = new BookStore(30, "newsprint");
+        }
+    }
+}
